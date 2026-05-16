c6a81aec6fcf0cd7180e20e53b46adcacb8ba9e7
FishDude936 <96391261+FishDude936@users.noreply.github.com>
2026-05-16 16:45:15 -0600

    Fixed Player Animation

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [Header("Object References")]
-    private PlayerController player;
     [SerializeField] TMP_Text scoreText;
-    [SerializeField] Transform hearts;
+    [SerializeField] TMP_Text live_counter;
     void Update()
     {
         scoreText.text = GameManager.instance.GetScore().ToString();
-        for (int i = 1; i < 4; i++)
-        {
-            hearts.Find(i.ToString()).gameObject.SetActive(GameManager.instance.lives >= i);
-        }
+        live_counter.text = $"x{GameManager.instance.lives}";
     }
 }
